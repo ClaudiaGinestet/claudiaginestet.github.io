3748357d68d1ffb86bc1dc177e2a0609ecf45566
ClaudiaAlvarez <claudia.alvarez402@gmail.com>
2024-04-27 17:45:31 -0300

    github pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   
+    $('#toggle-menu').click(function() {
+        $('nav ul').toggleClass('active');
+    });
+});
+$(document).ready(function () {
+    $('#carouselExampleSlidesOnly').carousel();
+});
