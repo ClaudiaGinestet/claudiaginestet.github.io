ba1685f8585b429570ef9b4f35a67df6f88c35ad
ClaudiaAlvarez <claudia.alvarez402@gmail.com>
2024-05-09 19:46:20 -0300

    reemplazo de archivos para clonar repositorio

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-   
-    $('#toggle-menu').click(function() {
-        $('nav ul').toggleClass('active');
-    });
-});
-$(document).ready(function () {
-    $('#carouselExampleSlidesOnly').carousel();
-});
