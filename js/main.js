d41e5d04f18dee7fb0c61550743c2bda4350d8e6
ClaudiaGinestet <106886243+ClaudiaGinestet@users.noreply.github.com>
2024-10-31 09:21:37 -0300

    js de cv boostrap

--- js/main.js
+++ js/main.js
@@ -1,119 +1,270 @@
-var fecha=new Date();  
-var anio=" - "+fecha.getUTCFullYear();  
-document.getElementById('fechafoot').innerHTML=anio; 
+/**
+* Template Name: MyResume
+* Updated: Nov 17 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
 
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const verificacion = urlParams.get('enviado')
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
 
-const NumeroTramite = document.getElementById("NumeroTramite");
-NumeroTramite.textContent = Math.floor(Math.random() * 10001);
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
 
-if (verificacion == "si"){
-    const MensajeFormulario = document.getElementById("MensajeFormulario");
-    MensajeFormulario.style.visibility = 'visible'
-}
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
 
-function validar() {
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
 
-    const IdentificacionTipo = document.getElementById("IdentificacionTipo").value;
-    const IdentificacionNumero = document.getElementById("IdentificacionNumero").value;
-    const IdentificacionError = document.getElementById("IdentificacionError");
-    IdentificacionError.textContent = "";
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
-    const Nombre = document.getElementById("Nombre").value;
-    const NombreError = document.getElementById("NombreError");
-    NombreError.textContent = "";
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
 
-    const Apellido = document.getElementById("Apellido").value;
-    const ApellidoError = document.getElementById("ApellidoError");
-    ApellidoError.textContent = "";
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
 
-    const Email = document.getElementById("Email").value;
-    const EmailError = document.getElementById("EmailError");
-    EmailError.textContent = "";
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
 
-    const Tele = document.getElementById("Tele").value;
-    const TeleError = document.getElementById("TeleError");
-    TeleError.textContent = "";
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
 
-    const Planes = document.getElementById("Planes").value;
-    const PlanesError = document.getElementById("PlanesError");
-    PlanesError.textContent = "";   
-    
-    const Fecha = document.getElementById("Fecha").value;
-    const FechaError = document.getElementById("FechaError");
-    FechaError.textContent = ""; 
+  /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 
-    const Provincia = document.getElementById("Provincia").value;
-    const ProvinciaError = document.getElementById("ProvinciaError");
-    ProvinciaError.textContent = "";  
-    
-    const Especialidad = document.getElementById("Especialidad").value;
-    const EspecialidadError = document.getElementById("EspecialidadError");
-    EspecialidadError.textContent = "";  
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
 
-    const Prepaga = document.getElementById("Prepaga").value;
-    const PrepagaError = document.getElementById("PrepagaError");
-    PrepagaError.textContent = "";
- 
-    
-    let esValido = true;
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
 
-    if (IdentificacionTipo === "") {
-        IdentificacionError.textContent = "Por favor seleccione su tipo de identificación.";
-        esValido = false;
-    }else if (IdentificacionNumero === "" || IdentificacionNumero.length < 7) {
-        IdentificacionError.textContent = "Por favor ingrese un número de identificación valido.";
-        esValido = false;
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
     }
 
-    if (Nombre === "" || /\d/.test(Nombre) || Nombre.length <3 ) {
-        NombreError.textContent = "Por favor ingrese un nombre valido.";
-        esValido = false;
-    }
+  });
 
-    if (Apellido === "" || /\d/.test(Apellido) || Apellido.length <3) {
-        ApellidoError.textContent = "Por favor ingrese un apellido valido.";
-        esValido = false;
-    }
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
 
-    if (Email === "" || !Email.includes("@")) {
-        EmailError.textContent ="Por favor ingrese un email valido.";
-        esValido = false;
-    }
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
 
-    if (Tele === "") {
-        TeleError.textContent = "Por favor seleccione su tipo de identificación.";
-        esValido = false;
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
     }
+  });
 
-    if (Prepaga === "" || Prepaga.length < 3) {
-        PrepagaError.textContent = "Por favor ingrese un valor valido.";
-        esValido = false;
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
     }
+  });
 
-    if (Planes === ""){
-        PlanesError.textContent = "Por favor ingrese un valor valido.";
-        esValido = false;
-    }
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
 
-    if (Fecha === "") {
-        FechaError.textContent = "Por favor seleccione una fecha valida.";
-        esValido = false;
-    }
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
 
-    if (Provincia === "") {
-        ProvinciaError.textContent = "Por favor seleccione una provincia valida.";
-        esValido = false;
-    }
-
-    if (Especialidad === "") {
-        EspecialidadError.textContent = "Por favor seleccione una especialidad valida.";
-        esValido = false;
-    }
-
-    return esValido;
-}
+})()
